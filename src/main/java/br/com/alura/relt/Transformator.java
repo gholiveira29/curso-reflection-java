82066c564fd52d26785f99c37adf19486c270ee8
gholiveira29 <g.h.p.o3615@gmail.com>
2025-08-13 19:12:19 -0300

    projeto curso reflection

---
+++ src/main/java/br/com/alura/relt/Transformator.java
@@ -0,0 +1,15 @@
+package br.com.alura.relt;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class Transformator {
+
+    public <I, O> O transform(I input) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class<?> source = input.getClass();
+        Class<?> target = source.forName(source.getClass() + "DTO");
+
+        O targetClass =  (O) target.getDeclaredConstructor().newInstance();
+
+        return targetClass;
+    }
+}
